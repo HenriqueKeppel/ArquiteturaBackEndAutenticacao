e9ebe4ec64e2a1e5b7923301ea2f7e0bcb646a7b
HenriqueKeppel <franzonikeppel@yahoo.com.br>
2018-10-04 00:27:08 -0300

    Primeira carga

---
+++ AutenticacaoAPI/Controllers/AutenticacaoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AutenticacaoAPI.ResponseModels;
+using AutenticacaoAPI.RequestModels;
+using AutenticacaoAPI.Models;
+
+namespace AutenticacaoAPI.Controllers
+{
+    [Route("AutenticacaoApi/v1/[controller]")]
+    public class AutenticacaoController : Controller
+    {
+        [HttpPost]
+        public async Task<AutenticacaoResponsePost> Post([FromBody]AutenticacaoRequestPost request)
+        {
+            AutenticacaoResponsePost result = new AutenticacaoResponsePost();
+
+            if (request.Email == "keppel@iec.com.br" && request.Pass == "123456")
+            {
+                AutenticacaoModel autenticacao = new AutenticacaoModel
+                {
+                    Token = Guid.NewGuid().ToString(),
+                    DataCriacao = DateTime.Now
+                };
+                result.StatusCode = 200;
+                result.Autenticacao = autenticacao;
+            }
+            else
+                result.StatusCode = 204;
+            return result;
+        }
+
+        
+        [HttpPost("ValidaAutenticacao")]
+        public async Task<bool> ValidaAutenticacao([FromBody]ValidaAutenticacaoRequestPost request)
+        {
+            return true;
+        }
+        
+    }
+}
---
+++ AutenticacaoAPI/Models/AutenticacaoModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace AutenticacaoAPI.Models
+{
+    public class AutenticacaoModel
+    {
+        public string Token {get;set;}
+        public DateTime DataCriacao {get;set;}
+    }
+}
